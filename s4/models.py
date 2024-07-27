6c79776da4a5b7b5f4f55b620d135c5f2cc4cf78
Dmitriy Shikhalev <d.shikhalev@innopolis.ru>
2024-07-28 04:46:08 +0500

    ...

--- s4/models.py
+++ s4/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from __future__ import annotations
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, List, relationship
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -9,20 +11,39 @@ class Base(MappedAsDataclass, DeclarativeBase):
     """subclasses will be converted to dataclasses"""
 
 
+storage_chunk_table = Table(
+    "association_table",
+    Base.metadata,
+    Column("storage_name", ForeignKey("storage.name"), primary_key=True),
+    Column("chunk_storage", ForeignKey("chunk.storage"), primary_key=True),
+)
+
+
+class Storage(Base):
+    __tablename__ = 'storage'
+
+    name: Mapped[str] = mapped_column(String(10), primary_key=True)
+
+    chunks: Mapped[List[Chunk]] = relationship(
+        secondary=storage_chunk_table, back_populates="storages"
+    )
+
+
 class File(Base):
     __tablename__ = 'files'
 
-    id: Mapped[int] = mapped_column(init=False, primary_key=True)
-    filename: Mapped[str]
+    filename: Mapped[str] = mapped_column(primary_key=True)
     size: Mapped[int]
-    hash: Mapped[str]
 
 
 class Chunk(Base):
     __tablename__ = 'chunks'
 
-    id = Column(Integer, primary_key=True)
-    file_id = Column(Integer, ForeignKey('files.id'))
-    no = Column(Integer)
-    hash = Column(String, nullable=True)
-    size = Column(Integer, nullable=True)
+    file_id = Column(Integer, ForeignKey('files.id'), primary_key=True)
+    number = Column(Integer, primary_key=True)
+    hash = Column(String)
+    size = Column(Integer)
+
+    storages: Mapped[List[Chunk]] = relationship(
+        secondary=storage_chunk_table, back_populates="chunks"
+    )
